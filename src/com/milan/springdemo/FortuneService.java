d9b5a3e047e7d00c6b1aeb723a30508ba821e259
Baelik <milan87ni@gmail.com>
2021-12-13 22:11:05 +0100

    constructor, setter method and value injection @qualifier annotation

---
+++ src/com/milan/springdemo/FortuneService.java
@@ -0,0 +1,6 @@
+package com.milan.springdemo;
+
+public interface FortuneService {
+
+	public String getFortune();
+}
---
+++ src/com/milan/springdemo/RandomFortuneService.java
@@ -0,0 +1,28 @@
+package com.milan.springdemo;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+	// create array of strings
+	private String[] data = {
+			"Beware of the wolf in the sheep's clothing",
+			"Diligance is the mother of good luck",
+			"The journey is the reward"
+	};
+	
+	// create a random number generator
+	private Random myRandom = new Random();
+	
+	@Override
+	public String getFortune() {
+		// pick a random string from array
+		int index = myRandom.nextInt(data.length);
+		String theFortune = data[index]; 
+		return theFortune;
+	}
+
+}
